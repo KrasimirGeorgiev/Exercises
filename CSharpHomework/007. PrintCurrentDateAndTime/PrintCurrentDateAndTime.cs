5f9d529f7094ce9a18095e7be8460c42d7bc6ec9
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2018-07-26 23:40:07 +0300

    HW
    More homework

---
+++ CSharpHomework/007. PrintCurrentDateAndTime/PrintCurrentDateAndTime.cs
@@ -0,0 +1,13 @@
+﻿namespace _007.PrintCurrentDateAndTime
+{
+    using System;
+
+    public class PrintCurrentDateAndTime
+    {
+        public static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+        }
+    }
+}
