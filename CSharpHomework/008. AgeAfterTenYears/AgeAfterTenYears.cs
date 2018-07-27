70f7bc748354cea36194b1b6adb8ed3636cb4dc8
Krasimir Georgiev <krasi.g.georgiev@gmail.com>
2018-07-27 22:23:32 +0300

    Solving task 7
    Solving task - age after ten years

---
+++ CSharpHomework/008. AgeAfterTenYears/AgeAfterTenYears.cs
@@ -0,0 +1,17 @@
+﻿namespace _008.AgeAfterTenYears
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AgeAfterTenYears
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write your birth day (in the format day/month/year)");
+            DateTime birthDay = DateTime.Parse(Console.ReadLine());
+        }
+    }
+}
